b745cfcd75a318f3386f71d81bc603012ed55e42
simoncbouchard <simoncbouchard@gmail.com>
2022-10-27 12:01:07 -0400

    Add cours-1-exercice

---
+++ cours-1-exercice/assets/scripts/main.js
@@ -0,0 +1,7 @@
+/**
+ * Vous avez 5 éléments DOM affichant un nombre,
+ * au click d'un des deux boutons à sa droite, faites le calcul (plus ou moins un) de ce nombre.
+ * Évidemment, vous avez à travailler en orienté objet en utilisant la syntaxe des classes ES6.
+ */
+
+
