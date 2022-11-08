df57634b2b5d25fd2032c46cb0498093cbc9d68a
simoncbouchard <simoncbouchard@gmail.com>
2022-11-08 09:04:02 -0500

    Add cours-3

---
+++ cours-3/4-module-patten-avec-prototype/Livre.js
@@ -0,0 +1,62 @@
+let Livre = (function() {
+
+    /**
+     * Section privée
+     */
+    // Variables privées
+    let nombrePage = 300,
+        pageActuelle = 1,
+        auteurPrenom = 'Tom',
+        auteurNom = 'Tucker';
+    
+
+    // Méthodes privées
+    function concateneNom(prenom, nom) {
+        return `${prenom} ${nom}`;
+    };
+
+
+
+    /**
+     * Section publique
+     */
+    // Méthodes publiques
+    function decrire() {    
+        return `Ce livre de ${this.auteur} a ${this.nombrePage} pages, je suis présentement rendu à la page ${this.pageActuelle}.`;
+    };
+    
+
+    // Constructeur
+    let Livre = function(valNbPage, valPageActuelle, valAuteurPrenom, valAuteurNom) {
+        this.nombrePage = valNbPage || nombrePage;
+        this.pageActuelle = valPageActuelle || pageActuelle;
+
+        if (valAuteurPrenom && valAuteurNom) this.auteur = concateneNom(valAuteurPrenom, valAuteurNom);
+        else this.auteur = concateneNom(auteurPrenom, auteurNom);
+    };
+    
+    
+    // Prototype
+    Livre.prototype = {
+        constructor: Livre,
+
+        // Méthodes publiques
+        avancePage: function() {                        
+            if (this.pageActuelle < this.nombrePage) {
+                this.pageActuelle++;
+            }
+        },
+        reculePage: function() {
+            if (this.pageActuelle > 0) {
+                this.pageActuelle--;
+            }
+        },
+
+        // Alias publique (public) pour appeler une fonction privée (private)
+        decrire: decrire
+    };
+
+
+    // Retourne l'objet avec son prototype
+    return Livre;
+})();
