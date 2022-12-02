5e25ee5d9c97634ff4a54831c3263d003890c750
simoncbouchard <simoncbouchard@gmail.com>
2022-12-02 07:55:50 -0500

    Add cours-10

---
+++ cours-10/2-localstorage/main.js
@@ -0,0 +1,27 @@
+(function() {
+
+    /**
+     * Ajouter un localStorage
+     */
+
+
+
+
+
+    /**
+     * Supprimer des données d'un localStorage (remove)
+     */
+
+
+
+
+
+    /**
+     * Supprimer toutes les données de localStorage (clear)
+     */
+
+
+
+
+
+})();
