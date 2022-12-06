45ef9ce3b0207d4b9162a70816ac4df32797f508
simoncbouchard <simoncbouchard@gmail.com>
2022-12-06 09:05:43 -0500

    Add cours-11-exercice

---
+++ cours-11-exercice/assets/scripts/main.js
@@ -0,0 +1,1 @@
+﻿// Lance l'instance de Ligue
---
+++ cours-11-exercice/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr_CA">
+<head>
+    <!-- meta -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Cours 11 | Exercice</title>
+	<meta name="description" content="Exercice cours 11 du cours 582-31F-MA Programmation d'interface Web 2 : API XMLHttpRequest">
+
+	<!-- styles -->
+    <link rel="stylesheet" type="text/css" href="./assets/styles/styles.css">
+    
+	<!-- scripts -->
+    <script type="module" src="./assets/scripts/main.js" defer></script>
+</head>
+
+<body>
+	<header>
+		<h1>Exercice cours 11</h1>
+	</header>
+
+	<main>
+
+		<div data-js-ligue>
+			<form>
+				<h3>Choisissez l'équipe :</h3>
+
+				<select>
+					<option value="0">Veuillez sélectionner une équipe</option>
+					<?php
+						
+					?>
+				</select>	
+
+			</form>
+			
+			<div data-js-joueurs></div>
+		</div>
+	</main>
+</body>
+</html>
